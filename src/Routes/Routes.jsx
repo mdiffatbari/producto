5680bcdeb58034abc3ca0681dbf0a92b83d49cb3
mdiffatbari <iffatbaribappy1999@gmail.com>
2026-03-26 11:35:56 +0600

    tailwind css and other setup done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home/Home";
+import Root from "../pages/Root/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+    ]
+  },
+]);
