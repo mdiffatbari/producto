55a4dc33bcff92e6b44c154f742db665804c9e1f
mdiffatbari <iffatbaribappy1999@gmail.com>
2026-03-26 16:43:07 +0600

    header, footer added, fully responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { NavLink } from "react-router";
+import logoImage from "../../assets/logo.png"
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
+
+
+    const links = <>
+        <div className='md:flex gap-3'>
+            <NavLink to="/"><li className='font-semibold text-[15px]'>Home</li></NavLink>
+            <NavLink to="/apps"><li className='font-semibold text-[15px]'>Apps</li></NavLink>
+            <NavLink to="/installation"><li className='font-semibold text-[15px]'>Installation</li></NavLink>
+        </div>
+    </>
+
     return (
         <div>
             <div className="navbar bg-base-100 shadow-sm">
@@ -12,36 +25,23 @@ const Navbar = () => {
                         <ul
                             tabIndex="-1"
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {links}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <div className='flex gap-1 items-center'>
+                        <img className='w-[40px]' src={logoImage} alt="logo" />
+                        <h1 className='text-2xl font-semibold'>Producto</h1>
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2 bg-base-100 w-40 z-1">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {links}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white border-none hover:opacity-90">
+                        <FaGithub /> Contribute
+                    </a>
                 </div>
             </div>
         </div>
